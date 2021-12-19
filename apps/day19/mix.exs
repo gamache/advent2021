cef5a5b507a956cee9103179ffd867f653292337
pete gamache <pete@gamache.org>
2021-12-19 09:43:07 -0500

    day 19 orientations

--- apps/day19/mix.exs
+++ apps/day19/mix.exs
@@ -9,7 +9,7 @@ defmodule Day19.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -25,6 +25,7 @@ defmodule Day19.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:matrex, "~> 0.6.8"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
